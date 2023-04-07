98541c4cda8beaf5b7fb754b88f9cf70e06515f9
Mostafa Salama <117605930+mostafasalama97@users.noreply.github.com>
2023-04-07 23:33:12 +0200

    Add files via upload

--- tests/unit/helpers.test.js
+++ tests/unit/helpers.test.js
@@ -13,53 +13,55 @@ it("getLicenseValidity", () => {
 })
 
 //========================================================================================================
-const { test } = require('ava');
+const { it } = require('ava');
 const { expect } = require('chai');
-test('transformArryToString should join array with the given delimiter', (t) => {
-  const array = ['apple', 'orange', 'banana'];
+it('transformArryToString should join expArr with the given delimiter', (t) => {
+  const expArr = ['apple', 'orange', 'banana'];
   const delimiter = '-';
-  const result = transformArryToString(delimiter, array);
+  const result = transformArryToString(delimiter, expArr);
   expect(result).to.equal('apple-orange-banana');
+  // console.log(result)
+
 });
 
-test('transformArryToString should return empty string when given an empty array', (t) => {
-  const array = [];
+it('transformArryToString should return empty string when given an empty expArr', (t) => {
+  const expArr = [];
   const delimiter = '-';
-  const result = transformArryToString(delimiter, array);
+  const result = transformArryToString(delimiter, expArr);
   expect(result).to.equal('');
 });
 
-test('transformArryToString should throw an error when given an invalid delimiter', (t) => {
-  const array = ['apple', 'orange', 'banana'];
+it('transformArryToString should throw an error when given an invalid delimiter', (t) => {
+  const expArr = ['apple', 'orange', 'banana'];
   const delimiter = 123;
-  expect(() => transformArryToString(delimiter, array)).to.throw('invalid delimeter');
+  expect(() => transformArryToString(delimiter, expArr)).to.throw('invalid delimeter');
 });
 
-test('transformArryToString should throw an error when delimiter is not provided', (t) => {
-  const array = ['apple', 'orange', 'banana'];
-  expect(() => transformArryToString(undefined, array)).to.throw('invalid delimeter');
+it('transformArryToString should throw an error when delimiter is not provided', (t) => {
+  const expArr = ['apple', 'orange', 'banana'];
+  expect(() => transformArryToString(undefined, expArr)).to.throw('invalid delimeter');
 });
 
-test('transformArryToString should throw an error when array is not provided', (t) => {
+it('transformArryToString should throw an error when expArr is not provided', (t) => {
   const delimiter = '-';
   expect(() => transformArryToString(delimiter)).to.throw('Cannot read property');
 });
 
-test('transformArryToString should throw an error when array is not an array', (t) => {
-  const array = 'not an array';
+it('transformArryToString should throw an error when expArr is not an expArr', (t) => {
+  const expArr = 'not an expArr';
   const delimiter = '-';
-  expect(() => transformArryToString(delimiter, array)).to.throw('array is not an array');
+  expect(() => transformArryToString(delimiter, expArr)).to.throw('expArr is not an expArr');
 });
 //==============================================================================================
 const sinon = require('sinon');
-test('getLicenseValidity returns valid when current year is before 2026', (t) => {
+it('getLicenseValidity returns valid when current year is before 2026', (t) => {
   const clock = sinon.useFakeTimers(new Date('2022-01-01').getTime());
   const result = getLicenseValidity();
   expect(result).to.equal('valid');
   clock.restore();
 });
 
-test('getLicenseValidity returns invalid when current year is after 2025', (t) => {
+it('getLicenseValidity returns invalid when current year is after 2025', (t) => {
   const clock = sinon.useFakeTimers(new Date('2026-01-01').getTime());
   const result = getLicenseValidity();
   expect(result).to.equal('invalid');
